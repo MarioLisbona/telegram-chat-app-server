38e08416aa0554fc1fd0fd18641a1e0bc1128564
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-04-29 12:35:08 +1000

    use env var for url host, port and env_node

--- init.js
+++ init.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 // create an express application instance
 const app = express();
@@ -7,7 +8,20 @@ const app = express();
 // create http instance using app
 const http = require("http").Server(app);
 
+// Determine the protocol based on environment
+const protocol = process.env.NODE_ENV === "production" ? "https://" : "http://";
+
+// connection variables for socket connection
+let clientHost = process.env.CLIENT_HOST || "localhost";
+let clientPort = process.env.CLIENT_PORT || 3000;
+
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: `${protocol}${clientHost}:${clientPort}`,
+  },
+});
+
 // use cors middleware
 app.use(cors());
 
-module.exports = { app, http };
+module.exports = { app, http, socketIO };
