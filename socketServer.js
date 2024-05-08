7fe02b39bedf94cd9186e524c7b70b9d56536f15
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-08 10:40:17 +1000

    emit original token query msg before price

--- socketServer.js
+++ socketServer.js
@@ -41,10 +41,13 @@ export function createSocketServer(bot) {
     socket.on("disconnect", () => {
       handleDisconnect(socket, socketIO, users);
     });
+  });
 
-    // event handler to receive "message" from telegram bot
-    bot.on("message", async (data) => {
-      handleTelegramMessage(socket, data);
-    });
+  // event handler to receive "message" from telegram bot
+  bot.on("message", async (data) => {
+    // emit message to all connected clients
+    socketIO.emit("telegramMessage", data);
+
+    handleTelegramMessage(socketIO, data, bot);
   });
 }
