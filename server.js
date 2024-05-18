69c8dd1d0a953d2b7fa9d573a5219735ee0d2b51
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-18 13:11:27 +1000

    Revert "Update cors middleware"
    This reverts commit 43e92f8fff180d781224aed4ebd4ec9b3e580bd6.

--- server.js
+++ server.js
@@ -16,25 +16,14 @@ setChatIdOnServer(bot, prisma);
 
 // Middleware to set CORS headers
 app.use((req, res, next) => {
-  const allowedOrigins = [
-    "https://telegram-chat-app.netlify.app",
-    // Add more origins if needed
-  ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Change * to the specific origin if needed
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
-  res.header("Access-Control-Allow-Credentials", true);
-  if (req.method === "OPTIONS") {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
 });
 
 //using app for home route
