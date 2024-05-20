aa91377bfd3328e9e8a9f183bb10105f255a73dd
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-21 05:58:40 +1000

    Use errorHandler middleware and update /messages

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import { setChatIdOnServer } from "./lib/socketUtils.js";
 import { createSocketServer } from "./socketServer.js";
 import { createTelegramBot } from "./telegramBot.js";
 import { getAllMessages } from "./lib/chatUtils.js";
+import { asyncHandler } from "./lib/asyncHandler.js";
+import { errorHandler } from "./lib/errorHandler.js";
 const PORT = process.env.PORT || 4000;
 
 // create an instance of the telegram bot
@@ -26,6 +28,9 @@ app.use((req, res, next) => {
   next();
 });
 
+// Use the error handler middleware
+app.use(errorHandler);
+
 //using app for home route
 app.get("/", (req, res) => {
   res.json({ message: "Hello from the Home Page" });
@@ -38,15 +43,13 @@ app.get("/chat", async function (req, res) {
   res.json(chatTitle);
 });
 
-app.get("/messages", (req, res) => {
-  getAllMessages()
-    .then((messages) => {
-      res.json(messages);
-    })
-    .catch((error) => {
-      console.error("Error fetching messages:", error);
-    });
-});
+app.get(
+  "/messages",
+  asyncHandler(async (req, res) => {
+    const messages = await getAllMessages();
+    res.json(messages);
+  })
+);
 
 // using http to listen on port
 httpServer.listen(PORT, () => {
