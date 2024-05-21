f75c88cb7123a1912287ef35956222b5a09f2c5d
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-21 11:31:06 +1000

    try-catch in createTelegramBot

--- telegramBot.js
+++ telegramBot.js
@@ -6,13 +6,18 @@ dotenvConfig();
 
 // function to create and return a new TelegramBot instance
 export const createTelegramBot = () => {
-  const env = process.env.NODE_ENV;
-  // assigning bot token from environment variables
-  const BOT_TOKEN =
-    env === "development" ? process.env.DEV_BOT_TOKEN : process.env.BOT_TOKEN;
+  try {
+    const env = process.env.NODE_ENV;
+    // assigning bot token from environment variables
+    const BOT_TOKEN =
+      env === "development" ? process.env.DEV_BOT_TOKEN : process.env.BOT_TOKEN;
 
-  // Create a bot instance
-  const bot = new TelegramBot(BOT_TOKEN, { polling: true });
+    // Create a bot instance
+    const bot = new TelegramBot(BOT_TOKEN, { polling: true });
 
-  return bot;
+    return bot;
+  } catch (error) {
+    console.error("Error in createTelegramBot:", error);
+    return { msg: `Error creating Telegram Bot: ${error.message}` };
+  }
 };
