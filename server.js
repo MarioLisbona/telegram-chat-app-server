dc625271c5d4cec7e8139282562f86d815725ccc
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-23 11:12:33 +1000

    Create route to server top 20 coins by marketCap

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import { createTelegramBot } from "./telegramBot.js";
 import { getAllMessages, getChatData } from "./lib/chatUtils.js";
 import { asyncHandler } from "./lib/asyncHandler.js";
 import { errorHandler, corsHandler } from "./lib/errorHandler.js";
-import { getFirstAndLast20Coins } from "./lib/tokenPrice.js";
+import { getCoinTickerData, getFirstAndLast20Coins } from "./lib/tokenPrice.js";
 const PORT = process.env.PORT || 4000;
 
 // create an instance of the telegram bot
@@ -55,6 +55,15 @@ app.get(
     console.log("Last 20 ->\n", lastTwentyPrices);
 
     res.json({ firstTwentyPrices, lastTwentyPrices });
+  })
+);
+
+app.get(
+  "/api/ticker",
+  asyncHandler(async (req, res) => {
+    const data = await getCoinTickerData();
+    const tickerData = data.slice(0, 20);
+    res.json(tickerData);
   })
 );
 
